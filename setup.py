7c8623a7a3a10ab88b6e0b907cb1cc3400675ba2
Riley Brady <riley.brady@colorado.edu>
2020-03-21 12:48:59 -0600

    add package files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='climpred_skeleton',
+      version='0.1',
+      description='Skeleton for climpred class inheritance',
+      url='http://github.com/bradyrx/climpred_skeleton',
+      license='MIT',
+      packages=['climpred_skeleton'],
+      zip_safe=False)
