66392eddb5719b6138881074320d8a9f7045297d
Riley Brady <riley.brady@colorado.edu>
2020-06-12 14:17:16 -0600

    finish hindcast scoring example

--- climpred_skeleton/scoring.py
+++ climpred_skeleton/scoring.py
@@ -1,3 +1,6 @@
+import xarray as xr
+from xskillscore import pearson_r
+
 from .alignment import LeadAlignment
 from .comparison import Comparison
 from .time import TimeManager
@@ -35,6 +38,34 @@ class HindcastScoring(Scoring):
         inits, verif_dates = alignment_obj._return_inits_and_verifs()
         self._scoring_inits = inits
         self._scoring_verifs = verif_dates
+
+    def _apply_metric_at_given_lead(self, lead):
+        fct = self._initialized
+        verif = self._observation
+        fct_inits = self._scoring_inits[lead]
+        all_inits = self._all_inits
+        # Use `.where()` instead of `.sel()` to account for resampled inits when
+        # bootstrapping.
+        a = (
+            fct.sel(lead=lead)
+            .where(all_inits.isin(fct_inits), drop=True)
+            .drop_vars('lead')
+            .rename({'init': 'time'})
+        )
+        b = verif.sel(time=self._scoring_verifs[lead])
+        a['time'] = b['time']
+        # Just example metric here.
+        return pearson_r(a, b, 'time')
+
+    def score(self):
+        metric_over_leads = [
+            self._apply_metric_at_given_lead(lead) for lead in self._leads
+        ]
+        result = xr.concat(
+            metric_over_leads, dim='lead', coords='minimal', compat='override'
+        )
+        result['lead'] = self._leads
+        return result
 
 
 class PerfectModelScoring(TimeManager):
