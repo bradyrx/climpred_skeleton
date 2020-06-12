c963bdf2539ed5f09601bc76d6235c6f4c68ef91
Riley Brady <riley.brady@colorado.edu>
2020-06-12 14:34:54 -0600

    add graphs and demo

--- demo.py
+++ demo.py
@@ -1,6 +1,7 @@
+from climpred import HindcastEnsemble
 from climpred.tutorial import load_dataset
 
-from climpred_skeleton.alignment import LeadAlignment
+from climpred_skeleton.scoring import HindcastScoring
 
 
 def main():
@@ -8,11 +9,12 @@ def main():
     NOTE: Make demo for PM as well.
     """
     hind = load_dataset('CESM-DP-SST')
+    hind['lead'].attrs['units'] = 'years'
     verif = load_dataset('FOSI-SST')
     print('\nBefore creating an object, init dimension is floats.\n')
     print(hind.init.head().to_index())
 
-    obj = LeadAlignment(hind, verif, 'e2o', 'same_verifs')
+    obj = HindcastScoring(hind, verif, 'e2o', 'same_inits')
     print('\nAfter instantiating, it is now in `cftime`.\n')
     print(obj._initialized.init.head().to_index())
 
@@ -21,6 +23,15 @@ def main():
 
     print('\nAfter creating object, comparison is run:\n')
     print(obj._initialized.dims)
+
+    print('\nScore can be run to automatically align and score the forecast:\n')
+    print(obj.score())
+
+    print('\nAnd it matches that from the current climpred:')
+    print(obj.score())
+    hindcast = HindcastEnsemble(hind)
+    hindcast = hindcast.add_observations(verif, 'fosi')
+    print(hindcast.verify(alignment='same_inits'))
 
 
 if __name__ == '__main__':
